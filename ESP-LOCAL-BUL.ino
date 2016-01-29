aa16cd934d91012a4dc574db2b430034688f69b2
kiralikbeyin@gmail.com <EvAkilli@ismail-MacBook-Pro.local>
2016-01-29 14:22:35 +0200

    FinfEspIpEvo
    FinfEspIpEvo

---
+++ ESP-LOCAL-BUL.ino
@@ -0,0 +1,100 @@
+ 
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+int status = WL_IDLE_STATUS;
+char ssid[] = "TTNET_ZyXEL_X4PT"; //  your network SSID (name)
+char pass[] = "cE76dEa12986e";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+unsigned int localPort = 4088;      // local port to listen on
+char packetBuffer[255]; //buffer to hold incoming packet
+ 
+
+WiFiUDP Udp;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+
+
+  Serial.println("I'm awake");
+  // attempt to connect to Wifi network:
+  WiFi.begin(ssid, pass);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  Udp.begin(localPort);
+
+  IPAddress ip = WiFi.localIP();
+ 
+}
+ 
+
+void loop() {
+
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+
+    //convert ip to char for udp reply
+    IPAddress ipi = WiFi.localIP();
+    String forchar =  String(ipi[0]) + "." + 
+                      String(ipi[1]) + "." + 
+                      String(ipi[2]) + "." + 
+                      String(ipi[3]);
+    char ReplyBuffer[16];
+    forchar.toCharArray(ReplyBuffer,16);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+}
+
+ 
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+
+}
+# FinfEspIpEvo
